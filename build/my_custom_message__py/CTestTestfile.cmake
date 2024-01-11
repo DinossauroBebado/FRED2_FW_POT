de46817bb18cc4e9ccb35c1db0979f968d52df58
DinossauroBebado <guipsilva32@gmail.com>
2024-01-11 15:42:21 -0300

    second time is the best

---
+++ build/my_custom_message__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dinossaurobebado/OPROJETO/Fred/FIrmware/FRED2_FW_POT/build/my_custom_message__py
+# Build directory: /home/dinossaurobebado/OPROJETO/Fred/FIrmware/FRED2_FW_POT/build/my_custom_message__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
